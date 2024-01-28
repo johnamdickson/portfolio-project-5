b431d4cf10ca683b5f17e84d9f7b17b51dfa2a1e
johnamdickson <johnamdickson@me.com>
2024-01-28 14:35:51 +0000

    build: add home app and index.html page

--- little_woolly_snuggles/settings.py
+++ little_woolly_snuggles/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 SITE_ID = 1
@@ -73,7 +74,10 @@ ROOT_URLCONF = 'little_woolly_snuggles.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
