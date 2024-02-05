4d656793d5f36c263939cce2386fb1bf6ca334bb
johnamdickson <johnamdickson@me.com>
2024-02-05 20:16:37 +0000

    build: add products app
    update settings and create Category and Product Models

---
+++ products/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    """
+    Category model for the product categories used in the store. Friendly name 
+    returns a more natural representation of the category.
+    """
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    unique_product_identifier = models.CharField(max_length=254, null=True, blank=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
+    learn_product = models.BooleanField(default=False, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
