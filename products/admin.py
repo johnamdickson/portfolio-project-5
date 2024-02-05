8c787788e337419c113c9d691216e4230ed1d9d6
johnamdickson <johnamdickson@me.com>
2024-02-05 21:45:14 +0000

    build: add product models to admin
    include list display data for both models

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Product, Category
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'unique_product_identifier',
+        'name',
+        'category',
+        'price',
+        'image',
+        'learn_product',
+        'has_sizes',
+    )
+
+    ordering = ('unique_product_identifier',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
