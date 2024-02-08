12b506fd82767a36ae243a4ea05e0e12ac923945
johnamdickson <johnamdickson@me.com>
2024-02-08 21:26:03 +0000

    feat: add popover for product sizes and colours
    use logic to determine if sizes or colours assigned and render a button which reveals detail in popover.

--- static/js/base.js
+++ static/js/base.js
@@ -1,8 +1,29 @@
 window.addEventListener('load', function () {
    styleNavBar();
-});
+   iniatilisePopover();
+   iniatiliseTooltips();
+})
 
 
+const iniatilisePopover = () => {
+    
+    
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+      return new bootstrap.Popover(popoverTriggerEl)
+    })
+
+    var popover = new bootstrap.Popover(document.querySelector('.popover-dismiss'), {
+        trigger: 'focus'
+      })
+}
+
+const iniatiliseTooltips = () => {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+}
 const styleNavBar = () => {
 /**
  * Dynamic styling of the navbar where the background color changes if the links are
