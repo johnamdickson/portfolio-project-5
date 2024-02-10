dd100e8e18aab3199ab7ddd549c918070bf556c5
johnamdickson <johnamdickson@me.com>
2024-02-10 03:22:43 +0000

    feat: add search bar functionality
    enable search of products using name and description columns

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Product, Category
+from django.contrib import messages
+from django.db.models import Q
 # Create your views here.
 
 def products(request):
@@ -8,9 +10,22 @@ def products(request):
     categorisation.
     """
     products = Product.objects.all().order_by('category')
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
