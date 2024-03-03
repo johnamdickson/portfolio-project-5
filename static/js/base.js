38e6a50d8a0c54d83aa86d5ac947bd324856d4f8
johnamdickson <johnamdickson@me.com>
2024-03-03 21:03:57 +0000

    feat: further off canvas implementation
    add register forms for small and medium up. Add logic to handle links between signup and login off canvases

--- static/js/base.js
+++ static/js/base.js
@@ -1,9 +1,9 @@
 window.addEventListener('load', function () {
-   styleNavBar();
-   iniatiliseTooltips();
-   iniatilisePopover();
-   initialiseToast();
-   offCanvasMethods();
+    styleNavBar();
+    iniatiliseTooltips();
+    iniatilisePopover();
+    initialiseToast();
+    offCanvasMethods();
 })
 
 // BS Boilerplate for initialising toasts.
@@ -22,26 +22,26 @@ const initialiseToast = () => {
 
 // BS boilerplate for initailising popover functionality
 const iniatilisePopover = () => {
-    
+
     var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-    var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
-      return new bootstrap.Popover(popoverTriggerEl)
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl)
     })
 }
 
 const iniatiliseTooltips = () => {
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
 }
 
 
 const styleNavBar = () => {
-/**
- * Dynamic styling of the navbar where the background color changes if the links are
- * hovered over as opposed to the navbar itself.
- */
+    /**
+     * Dynamic styling of the navbar where the background color changes if the links are
+     * hovered over as opposed to the navbar itself.
+     */
     // check pathname to determine when not on home page and prevent console errors.
     // https://www.skillsugar.com/check-if-homepage-javascript?utm_content=cmp-true
     current = window.location.pathname;
@@ -55,7 +55,7 @@ const styleNavBar = () => {
                 // hide dropdown when mouseleaves the element.
                 dropdown.onmouseleave = () => {
                     var bsCollapse = new bootstrap.Collapse(dropdown, {
-                        toggle: true,                    
+                        toggle: true,
                     })
                     let navLinks = dropdown.closest('.nav-item').getElementsByClassName('nav-link');
                     for (let navLink of navLinks) {
@@ -63,7 +63,7 @@ const styleNavBar = () => {
                         // before this particular link will work again.
                         navLink.classList.remove('show');
                     }
-                    };
+                };
                 dropdown.style.backgroundColor = 'rgba(129,168,166,0.97)';
                 dropdown.style.color = '#fff'
                 dropdown.classList.add('shadow');
@@ -76,25 +76,25 @@ const styleNavBar = () => {
                     link.style.fontWeight = '200';
                     // style background link on mouseover along with the parent navlink.
                     link.onmouseover = (event) => {
-                    for (let navLink of navLinks){
-                        navLink.style.color = '#eddfe7';
-                        navLink.style.fontSize = '1.25rem';
-                        navLink.style.fontWeight = '400';
+                        for (let navLink of navLinks) {
+                            navLink.style.color = '#eddfe7';
+                            navLink.style.fontSize = '1.25rem';
+                            navLink.style.fontWeight = '400';
                         }
-                    link.style.color = '#eddfe7';
-                    link.style.fontWeight = '400';
-                    event.target.style.backgroundColor = '#4d6562';
+                        link.style.color = '#eddfe7';
+                        link.style.fontWeight = '400';
+                        event.target.style.backgroundColor = '#4d6562';
                     };
                     // return style to normal on mouseleave event.
                     link.onmouseleave = (event) => {
-                        for (let navLink of navLinks){
+                        for (let navLink of navLinks) {
                             navLink.style.color = '#fff'
                             navLink.style.fontSize = 'clamp(1rem, 2vw, 1.1rem)';
-                            }
+                        }
                         link.style.color = '#fff';
                         link.style.fontWeight = '200';
                         event.target.style.backgroundColor = 'transparent';
-                        };
+                    };
                 }
             };
             // style the main navbar links on mouseenter and leave.
@@ -112,11 +112,11 @@ const styleNavBar = () => {
                     navBar.classList.add('shadow-sm');
                     link.style.color = '#f1d5e5';
                     link.style.fontWeight = '400';
-                    };
+                };
                 link.onmouseleave = () => {
-                        link.style.color = '#fff';
-                        link.style.fontWeight = '200';
-                    };
+                    link.style.color = '#fff';
+                    link.style.fontWeight = '200';
+                };
             };
             navBar.onmouseleave = () => {
                 navBar.classList.remove('shadow-sm');
@@ -127,13 +127,13 @@ const styleNavBar = () => {
 
 
 window.onscroll = () => {
- /**
- * use of onscroll function to adjust visibility of back to top
- * button. Once window scrolls down to and past 5px the button is visible and
- * hidden once the window scrolls back above 5px.
- */
+    /**
+     * use of onscroll function to adjust visibility of back to top
+     * button. Once window scrolls down to and past 5px the button is visible and
+     * hidden once the window scrolls back above 5px.
+     */
     let topButton = document.getElementById('back-to-top')
-    if (window.scrollY<=5) {
+    if (window.scrollY <= 5) {
         topButton.style.visibility = 'hidden'
     } else {
         topButton.style.visibility = 'visible'
@@ -150,43 +150,68 @@ const offCanvasMethods = () => {
     const accountOffcanvas = document.getElementById('offcanvas-menu-account')
     const accountOffcanvasSmall = document.getElementById('offcanvas-menu-account-sm')
 
-    // set up log in off canvases from bottom on small screens and from right on medium and above.
     const loginOffcanvas = document.getElementById('offcanvas-menu-login')
     const loginOffcanvasSmall = document.getElementById('offcanvas-menu-login-sm')
-    const loginButtons = Array.from(document.getElementsByClassName('offcanvas-login-button'))
-    const loginUsernameInput = document.getElementsByClassName('login-username-input')
+    const loginUsernameInputSmall = document.getElementById('id_login_1')
+    const loginUsernameInput = document.getElementById('id_login')
+    let loginButtons = Array.from(document.getElementsByClassName('offcanvas-login-button'))
+
+    const logoutOffcanvas = document.getElementById('offcanvas-menu-logout')
+    const logoutOffcanvasSmall = document.getElementById('offcanvas-menu-logout-sm')
+    let logoutButtons = Array.from(document.getElementsByClassName('offcanvas-logout-button'))
+
+    const registerOffcanvas = document.getElementById('offcanvas-menu-register')
+    const registerOffcanvasSmall = document.getElementById('offcanvas-menu-register-sm')
+    let registerButtons = Array.from(document.getElementsByClassName('offcanvas-register-button'))
+    const registerEmailInput = document.getElementById('id_email')
+    const registerEmailInputSmall = document.getElementById('id_email_1')
+
     const delayInMilliseconds = 500
 
-    for (let [i, button] of loginButtons.entries()) {
-        
-        button.onclick = () => {
-            
+    // set up log in off canvases from bottom on small screens and from right on medium and above.
 
-            if (i == 0) {
+    for (let [i, button] of loginButtons.entries()) {
+        button.onclick = () => {
+            if (i === 0) {
                 const account = bootstrap.Offcanvas.getInstance(accountOffcanvasSmall)
                 account.hide()
                 const login = new bootstrap.Offcanvas(loginOffcanvasSmall)
                 login.show()
-                setTimeout(function() {
-                    loginUsernameInput[i].focus()
-                  }, delayInMilliseconds);
-            } else {
+                setTimeout(function () {
+                    loginUsernameInputSmall.focus()
+                }, delayInMilliseconds);
+            } else if (i === 1) {
+                const register = bootstrap.Offcanvas.getInstance(registerOffcanvasSmall)
+                register.hide()
+                const login = new bootstrap.Offcanvas(loginOffcanvasSmall)
+                login.show()
+                setTimeout(function () {
+                    loginUsernameInputSmall.focus()
+                }, delayInMilliseconds);
+            } else if (i === 2) {
                 const account = bootstrap.Offcanvas.getInstance(accountOffcanvas)
                 account.hide()
                 const login = new bootstrap.Offcanvas(loginOffcanvas)
                 login.show()
-                setTimeout(function() {
-                    loginUsernameInput[i].focus()
-                  }, delayInMilliseconds);
+                setTimeout(function () {
+                    loginUsernameInput.focus()
+                }, delayInMilliseconds);
+            } else {
+                const register = bootstrap.Offcanvas.getInstance(registerOffcanvas)
+                register.hide()
+                const login = new bootstrap.Offcanvas(loginOffcanvas)
+                login.show()
+                setTimeout(function () {
+                    loginUsernameInput.focus()
+                }, delayInMilliseconds);
             }
         }
     }
-    const logoutOffcanvas = document.getElementById('offcanvas-menu-logout')
-    const logoutOffcanvasSmall = document.getElementById('offcanvas-menu-logout-sm')
-    let logoutButtons = Array.from(document.getElementsByClassName('offcanvas-logout-button'))
-    
+
+    // set up logout off canvases from bottom on small screens and from right on medium and above.
+
     for (let [i, button] of logoutButtons.entries()) {
-        
+
         button.onclick = () => {
             if (i == 0) {
                 const account = bootstrap.Offcanvas.getInstance(accountOffcanvasSmall)
@@ -201,5 +226,45 @@ const offCanvasMethods = () => {
             }
         }
     }
-}
 
+
+    // set up register off canvases from bottom on small screens and from right on medium and above.
+
+    for (let [i, button] of registerButtons.entries()) {
+        button.onclick = () => {
+            if (i == 0) {
+                const account = bootstrap.Offcanvas.getInstance(accountOffcanvasSmall)
+                account.hide()
+                const register = new bootstrap.Offcanvas(registerOffcanvasSmall)
+                register.show()
+                setTimeout(function () {
+                    registerEmailInputSmall.focus()
+                }, delayInMilliseconds);
+            } else if (i === 1) {
+                const login = bootstrap.Offcanvas.getInstance(loginOffcanvasSmall)
+                login.hide()
+                const register = new bootstrap.Offcanvas(registerOffcanvasSmall)
+                register.show()
+                setTimeout(function () {
+                    registerEmailInputSmall.focus()
+                }, delayInMilliseconds);
+            } else if (i === 2) {
+                const account = bootstrap.Offcanvas.getInstance(accountOffcanvas)
+                account.hide()
+                const register = new bootstrap.Offcanvas(registerOffcanvas)
+                register.show()
+                setTimeout(function () {
+                    registerEmailInput.focus()
+                }, delayInMilliseconds);
+            } else {
+                const login = bootstrap.Offcanvas.getInstance(loginOffcanvas)
+                login.hide()
+                const register = new bootstrap.Offcanvas(registerOffcanvas)
+                register.show()
+                setTimeout(function () {
+                    registerEmailInput.focus()
+                }, delayInMilliseconds);
+            }
+        }
+    }
+}
