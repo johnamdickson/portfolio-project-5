a177d4ef3fccf5f0a256ba858e60f8b591714bca
johnamdickson <johnamdickson@me.com>
2024-03-12 22:55:52 +0000

    refactor: update webhook handler
    include item properties for line item objects

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.conf import settings
@@ -17,22 +17,22 @@ class StripeWH_Handler:
     def __init__(self, request):
         self.request = request
 
-    def _send_confirmation_email(self, order):
-        """Send the user a confirmation email"""
-        cust_email = order.email
-        subject = render_to_string(
-            'checkout/confirmation_emails/confirmation_email_subject.txt',
-            {'order': order})
-        body = render_to_string(
-            'checkout/confirmation_emails/confirmation_email_body.txt',
-            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+    # def _send_confirmation_email(self, order):
+    #     """Send the user a confirmation email"""
+    #     cust_email = order.email
+    #     subject = render_to_string(
+    #         'checkout/confirmation_emails/confirmation_email_subject.txt',
+    #         {'order': order})
+    #     body = render_to_string(
+    #         'checkout/confirmation_emails/confirmation_email_body.txt',
+    #         {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
 
-        send_mail(
-            subject,
-            body,
-            settings.DEFAULT_FROM_EMAIL,
-            [cust_email]
-        )
+    #     send_mail(
+    #         subject,
+    #         body,
+    #         settings.DEFAULT_FROM_EMAIL,
+    #         [cust_email]
+    #     )
 
     def handle_event(self, event):
         """
@@ -60,7 +60,6 @@ class StripeWH_Handler:
         billing_details = stripe_charge.billing_details # updated
         shipping_details = intent.shipping
         grand_total = round(stripe_charge.amount / 100, 2) # updated
-
         # Clean data in the shipping details
         for field, value in shipping_details.address.items():
             if value == "":
@@ -92,7 +91,7 @@ class StripeWH_Handler:
                 attempt += 1
                 time.sleep(1)
         if order_exists:
-            self._send_confirmation_email(order)
+            # self._send_confirmation_email(order)
             return HttpResponse(
                 content=(f'Webhook received: {event["type"]} | SUCCESS: '
                          'Verified order already in database'),
@@ -101,8 +100,9 @@ class StripeWH_Handler:
             order = None
             try:
                 order = Order.objects.create(
+                    order_number=order_number,
                     full_name=shipping_details.name,
-                    user_profile=profile,
+                    # user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
@@ -111,8 +111,6 @@ class StripeWH_Handler:
                     street_address1=shipping_details.address.line1,
                     street_address2=shipping_details.address.line2,
                     county=shipping_details.address.state,
-                    original_cart=cart,
-                    stripe_pid=pid,
                 )
                 for item_id, item_data in json.loads(cart).items():
                     product = Product.objects.get(id=item_id)
@@ -124,25 +122,35 @@ class StripeWH_Handler:
                         )
                         order_line_item.save()
                     else:
-                        for size, quantity in item_data['items_by_size'].items():
+                        for properties, quantity in item_data['items_size_and_or_colour'].items():
+                            property_list = properties.split(',')
+                            size = property_list[0]
+                            size = None if size == 'None' else size
+                            colour = property_list[1]
+                            colour = None if colour == 'None' else colour
+                            secondary_colour = property_list[2]
+                            secondary_colour = None if secondary_colour == 'None' else secondary_colour  
                             order_line_item = OrderLineItem(
                                 order=order,
                                 product=product,
                                 quantity=quantity,
                                 product_size=size,
+                                product_primary_colour = colour,
+                                product_secondary_colour = secondary_colour,
                             )
-                            order_line_item.save()
+                        order_line_item.save()
             except Exception as e:
                 if order:
                     order.delete()
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
-        self._send_confirmation_email(order)
+        # self._send_confirmation_email(order)
         return HttpResponse(
             content=(f'Webhook received: {event["type"]} | SUCCESS: '
                      'Created order in webhook'),
             status=200)
+            
 
     def handle_payment_intent_payment_failed(self, event):
         """
