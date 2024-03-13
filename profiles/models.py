91ccb4bd6dda5c38a772a5fbe4ddd7a82926d295
johnamdickson <johnamdickson@me.com>
2024-03-13 22:10:56 +0000

    feat: update profile template
    add order history and form to profile page. Update views and models to achieve functionality

--- profiles/models.py
+++ profiles/models.py
@@ -11,7 +11,9 @@ class UserProfile(models.Model):
     A user profile model for maintaining default
     delivery information and order history
     """
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE,
+        verbose_name='User')
     default_phone_number = models.CharField(max_length=20,
                                             null=True, blank=True,
                                             verbose_name='Phone Number')
@@ -38,12 +40,12 @@ class UserProfile(models.Model):
         return self.user.username
 
 
-# @receiver(post_save, sender=User)
-# def create_or_update_user_profile(sender, instance, created, **kwargs):
-#     """
-#     Create or update the user profile
-#     """
-#     if created:
-#         UserProfile.objects.create(user=instance)
-#     # Existing users: just save the profile
-#     instance.userprofile.save()
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create or update the user profile
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+    # Existing users: just save the profile
+    instance.userprofile.save()
