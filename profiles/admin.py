1feece6905262c3634d08398cf76068ef2b357d5
johnamdickson <johnamdickson@me.com>
2024-03-13 04:18:40 +0000

    build: add user profile admin class

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+
+    fields = ('user', 
+            'default_phone_number',
+            'default_postcode',
+            'default_town_or_city',
+            'default_street_address1',
+            'default_street_address2',
+            'default_county',
+            'default_country',
+        )
+
+    list_display = ('user',)
+
+
+admin.site.register(UserProfile, ProfileAdmin)
