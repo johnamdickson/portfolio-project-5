be31e80fea36439ee85524c66c07a40cd0b3e602
johnamdickson <johnamdickson@me.com>
2024-03-13 04:20:25 +0000

    refactor: add verbose name to profile fields

--- profiles/models.py
+++ profiles/models.py
@@ -13,30 +13,37 @@ class UserProfile(models.Model):
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20,
-                                            null=True, blank=True)
+                                            null=True, blank=True,
+                                            verbose_name='Phone Number')
     default_street_address1 = models.CharField(max_length=80,
-                                               null=True, blank=True)
+                                               null=True, blank=True,
+                                               verbose_name='Address 1')
     default_street_address2 = models.CharField(max_length=80,
-                                               null=True, blank=True)
+                                               null=True, blank=True,
+                                               verbose_name='Address 2')
     default_town_or_city = models.CharField(max_length=40,
-                                            null=True, blank=True)
+                                            null=True, blank=True,
+                                               verbose_name='Town/City')
     default_county = models.CharField(max_length=80,
-                                      null=True, blank=True)
+                                      null=True, blank=True,
+                                      verbose_name='County')
     default_postcode = models.CharField(max_length=20,
-                                        null=True, blank=True)
+                                        null=True, blank=True,
+                                        verbose_name='Postcode')
     default_country = CountryField(blank_label='Country',
-                                   null=True, blank=True)
+                                   null=True, blank=True,
+                                   verbose_name='Country')
 
     def __str__(self):
         return self.user.username
 
 
-@receiver(post_save, sender=User)
-def create_or_update_user_profile(sender, instance, created, **kwargs):
-    """
-    Create or update the user profile
-    """
-    if created:
-        UserProfile.objects.create(user=instance)
-    # Existing users: just save the profile
-    instance.userprofile.save()
+# @receiver(post_save, sender=User)
+# def create_or_update_user_profile(sender, instance, created, **kwargs):
+#     """
+#     Create or update the user profile
+#     """
+#     if created:
+#         UserProfile.objects.create(user=instance)
+#     # Existing users: just save the profile
+#     instance.userprofile.save()
