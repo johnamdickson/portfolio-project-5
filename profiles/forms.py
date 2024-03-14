822a1f7a294cb5fc4e00aacb263961801da7800d
johnamdickson <johnamdickson@me.com>
2024-03-14 00:51:45 +0000

    fix: exclude user from UserProfile form
    stop form not valid errors when user input disabled

--- profiles/forms.py
+++ profiles/forms.py
@@ -5,5 +5,8 @@ from .models import UserProfile
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        exclude = ()
+        exclude = ('user',)
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
