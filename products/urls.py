398a9237069e2b67499fbfe2973bbc0f11d65bcd
johnamdickson <johnamdickson@me.com>
2024-03-17 01:24:00 +0000

    feat: add edit product functionality
    add view method, url and template along with associated styling

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.products, name='products'),
     path('<int:product_pk>/', views.product_detail, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
 ]
