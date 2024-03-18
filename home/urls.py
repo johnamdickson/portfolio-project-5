608f59d625d4b42b7e911b2e869f056304ac1bfd
johnamdickson <johnamdickson@me.com>
2024-03-18 14:57:23 +0000

    feat: add testing functionality for HTTP errors

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('test-500', views.simulate_500_error_view, name='500'),
+    path('test-400', views.simulate_400_error_view, name='400'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.http import HttpResponseServerError, HttpResponseBadRequest
 
 # Create your views here.
 
@@ -6,3 +7,15 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+def simulate_500_error_view(request):
+    if request.user.is_superuser:
+        return HttpResponseServerError("500 Internal Server Error")
+    else:
+        return redirect(reverse('home'))
+
+def simulate_400_error_view(request):
+    if request.user.is_superuser:
+        return HttpResponseBadRequest("400 Bad Request Error")
+    else:
+        return redirect(reverse('home'))
