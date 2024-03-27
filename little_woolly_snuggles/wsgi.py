36a70b8adabe94f1a990a5ef3cb6efcf3c8287c8
johnamdickson <johnamdickson@me.com>
2024-03-27 21:34:51 +0000

    fix: csrf 403 error in incongnito mode

--- little_woolly_snuggles/wsgi.py
+++ little_woolly_snuggles/wsgi.py
@@ -13,4 +13,13 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'little_woolly_snuggles.settings')
 
-application = get_wsgi_application()
+django_app = get_wsgi_application()
+
+
+
+def https_app(environ, start_response):
+    environ["wsgi.url_scheme"] = "https"
+    return django_app(environ, start_response)
+
+
+application = https_app
