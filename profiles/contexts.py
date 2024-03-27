fe7733bb0008fde18dededda8b03b79c1ddabf6f
johnamdickson <johnamdickson@me.com>
2024-03-27 09:42:25 +0000

    test: further updates from profile validation

--- profiles/contexts.py
+++ profiles/contexts.py
@@ -12,12 +12,12 @@ def profile_details(request):
     if request.user.is_authenticated:
         """ Display the user's profile. """
         profile = get_object_or_404(UserProfile, user=request.user)
-        form_regular = UserProfileForm(instance=profile, prefix='offcanvas_form_regular')
-        form_small = UserProfileForm(instance=profile, prefix='offcanvas_form_small')
+        form_regular = UserProfileForm(instance=profile, prefix='profile_offcanvas_form_regular')
+        form_small = UserProfileForm(instance=profile, prefix='profile_offcanvas_form_small')
         orders = profile.orders.all().order_by('-date')
         context = {
-            'form_small': form_small,
-            'form_regular': form_regular,
+            'profile_form_small': form_small,
+            'profile_form_regular': form_regular,
             'orders': orders,
             'on_profile_page': True
         }
